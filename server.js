54efcb78cd91f346dd232111f5f95bddfaac44f8
Kelly Nelson <kelnel86@gmail.com>
2022-11-29 12:21:12 -0600

    connecting mongoDB

--- server.js
+++ server.js
@@ -3,6 +3,37 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require("cors");
+const mongoose = require('mongoose');
+
+/////// Database Connection /////////
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+////// Database Connection Error/Success//////////////
+////// Define callback functions for various events/////////////
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+/////// Routes ////////////
+
+////// Index /////////
+app.get('/recipes', (req, res) => {
+    res.send('index page')
+})
+
+//////Create /////////
+app.post('/recipes', (req, res) => {
+    res.send('received')
+})
+
+
+
+
 
 /////// Listener ///////////
 const PORT = process.env.PORT;
